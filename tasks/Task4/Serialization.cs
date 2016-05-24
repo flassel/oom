d9e5a413938aa0a2f0e6c2f89cb7eeb9b9d05089
Frederic Lassel <frederic.lassel@live.at>
2016-05-24 19:48:07 +0200

    Task4

--- tasks/Task4/Serialization.cs
+++ tasks/Task4/Serialization.cs
@@ -10,25 +10,17 @@ namespace Task4
         {
             var notebook = products[0];
 
-            // 1. serialize a single book to a JSON string
-            Console.WriteLine(JsonConvert.SerializeObject(notebook));
-
-            // 2. ... with nicer formatting
-            Console.WriteLine(JsonConvert.SerializeObject(notebook, Formatting.Indented));
-
-            // 3. serialize all items
-            // ... include type, so we can deserialize sub-classes to interface type
+            // serialize
             var settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
             Console.WriteLine(JsonConvert.SerializeObject(products, settings));
-
-            // 4. store json string to file "items.json" on your Desktop
+            
+            // store to desktop
             var text = JsonConvert.SerializeObject(products, settings);
             var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             var filename = Path.Combine(desktop, "products.json");
             File.WriteAllText(filename, text);
 
-            // 5. deserialize items from "items.json"
-            // ... and print Description and Price of deserialized items
+            // deserialize from desktop file
             var textFromFile = File.ReadAllText(filename);
             var itemsFromFile = JsonConvert.DeserializeObject<Product[]>(textFromFile, settings);
             var currency = Currency.EUR;
